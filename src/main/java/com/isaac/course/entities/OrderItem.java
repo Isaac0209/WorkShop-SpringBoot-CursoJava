479173597f5d71f3509805279b119c6914ddbdb9
Isaac Roque <isaacroque0209@gmail.com>
2024-02-19 19:48:35 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/isaac/course/entities/OrderItem.java
@@ -0,0 +1,74 @@
+package com.isaac.course.entities;
+
+import java.util.Objects;
+
+import com.isaac.course.entities.pk.OrdemItemPK;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem {
+
+	@EmbeddedId
+	private OrdemItemPK id;
+	
+	private Double price;
+	
+	private Integer quantity;
+
+	public OrderItem() {
+		
+	}
+	public OrderItem(Order order, Product product, Double price, Integer quantity) {
+		super();
+		id.setOrder(order);
+		id.setProduct(product);
+		this.price = price;
+		this.quantity = quantity;
+	}
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+}
