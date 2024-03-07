f02cf0b0afda1d645baf46198aa4a03632782798
Isaac Roque <isaacroque0209@gmail.com>
2024-03-07 19:58:28 -0300

    Exception handling - delete

--- src/main/java/com/isaac/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/isaac/course/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.isaac.course.services.exceptions.DatabaseException;
 import com.isaac.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,6 +20,13 @@ public class ResourceExceptionHandler {
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError se = new StandardError(Instant.now(), status.value(), error, rs.getMessage(), hsr.getRequestURI());
+		return ResponseEntity.status(status).body(se);
+	}
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> databaseException(DatabaseException db, HttpServletRequest request){
+		String error = "Database Error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError se = new StandardError(Instant.now(), status.value(), error, db.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(se);
 	}
 }
--- src/main/java/com/isaac/course/services/UserService.java
+++ src/main/java/com/isaac/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.isaac.course.entities.User;
 import com.isaac.course.repositories.UserRepository;
+import com.isaac.course.services.exceptions.DatabaseException;
 import com.isaac.course.services.exceptions.ResourceNotFoundException;
 @Service
 public class UserService {
@@ -25,7 +28,14 @@ public class UserService {
 		return userRepository.save(obj);
 	}
 	public void delete(Long id) {
-		userRepository.deleteById(id);
+		try {
+			userRepository.deleteById(id);
+
+		}catch(EmptyResultDataAccessException err) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException err) {
+			throw new DatabaseException(err.getMessage());
+		}
 	}
 	public User update(Long id, User obj) {
 		User entity = userRepository.getReferenceById(id);
